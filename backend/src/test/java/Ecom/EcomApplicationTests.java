8386568d612940993a0e92c499448a2849a8ccf6
sandesh300 <bhujbalsandesh52@gmail.com>
2026-02-22 18:40:02 +0530

    update code 1

--- backend/src/test/java/Ecom/EcomApplicationTests.java
+++ backend/src/test/java/Ecom/EcomApplicationTests.java
@@ -2,12 +2,14 @@ package Ecom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY) // Replaces the DataSource with an embedded H2
 class EcomApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
